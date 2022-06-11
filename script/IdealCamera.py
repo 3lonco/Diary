6770eaa402c2fb931a80743113c6adc99c9335eb
Jun Kubota <kinkado115@gmail.com>
2022-06-11 16:25:46 +0900

    IdealCameraの追加

--- script/IdealCamera.py
+++ script/IdealCamera.py
@@ -1,18 +1,24 @@
 import numpy as np
+import math
+
+
 class IdealCamera:
-  def __init__(self,env_map):
-    self.map=env_map
+    def __init__(self, env_map):
+        self.map = env_map
 
-  def data(self,cam_pose):
-    observed=[]
-    for im in self.map.landmarks:
-      p=self.observation_function(cam_pose,im_pos)
+    def data(self, cam_pose):
+        observed = []
+        for Im in self.map.landmarks:
+            p = self.observation_function(cam_pose, Im.pos)
+            observed.append((p, Im.id))
+        return observed
 
-
-
-
-  @classmethod
-  def observation_function(cls,cam_pose,obj_pos):
-    diff = obj_pos - cam_pose[0:2]
-    phi = math.atan2(diff[1],diff[0])-cam_pose[2]
-    while phi >=np.pi:phi 
+    @classmethod
+    def observation_function(cls, cam_pose, obj_pos):
+        diff = obj_pos - cam_pose[0:2]
+        phi = math.atan2(diff[1], diff[0]) - cam_pose[2]
+        while phi >= np.pi:
+            phi -= 2 * np.pi
+        while phi < -np.pi:
+            phi += 2 * np.pi
+        return np.array([np.hypot(*diff), phi]).T
--- script/map.py
+++ script/map.py
@@ -1,12 +1,14 @@
 import Landmark
 
-class Map:
-  def __init__(self):
-    self.landmarks=[]
-  def append_landmark(self,landmark):
-    landmark.id = len(self.landmarks)
-    self.landmarks.append(landmark)
 
-  def draw(self,ax,elems):
-    for Im in self.landmarks:
-      Im.draw(ax,elems)
+class Map:
+    def __init__(self):
+        self.landmarks = []
+
+    def append_landmark(self, landmark):
+        landmark.id = len(self.landmarks)
+        self.landmarks.append(landmark)
+
+    def draw(self, ax, elems):
+        for Im in self.landmarks:
+            Im.draw(ax, elems)
