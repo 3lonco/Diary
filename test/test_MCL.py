61550128df6b92b275bdc2512d63db36f54f9eb6
Jun Kubota <kinkado115@gmail.com>
2022-06-16 20:53:48 +0900

    test

--- test/test_MCL.py
+++ test/test_MCL.py
@@ -1,6 +1,8 @@
-from ..script import world
-
-
+# Add path for CI/CD tool
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + "/../script/"))
+import world
 def add(x, y):
     return x + y
 
@@ -12,5 +14,4 @@ def div(x, y):
 def test_add():
     res = add(1, 2)
     w = world.World()
-
     assert res == 3
